7446295738a5bf4e99eaa9e8c6dc4e566afc2868
Harsha-Vardhini-Sujanmulk <134298101+Harsha-Vardhini-Sujanmulk@users.noreply.github.com>
2023-06-05 15:06:03 +0530

    Add files via upload

---
+++ JS FREECODECAMP/Practice/templateStrings.js
@@ -0,0 +1,8 @@
+const recipient = "James";
+let sender = "Honey";
+const email1 = "Hey " + recipient + "! How is it going? Cheers Per";
+
+const email2 = `Hey ${recipient}! How is it going? Cheers ${sender}`;
+
+console.log(email1);
+console.log(email2);
---
+++ JavaScript/DRUM KIT PROJECT/DOM SAMPLE CODES/sample.js
@@ -0,0 +1,39 @@
+// PART - 1
+
+// ways of accessing html elements
+// document.getElementsByTagName("li");
+// document.getElementsByTagName("li").style.color = "pink"          generates an error
+// document.getElementsByTagName("li")[2].style.color = "pink";
+// document.getElementsByTagName("li").length; // 3
+// document.getElementsByClassName("list");
+// document.getElementById("id1");
+// document.getElementById("id1").innerHTML = "bye";
+// document.getElementById("id1");
+// document.querySelector("h1");
+// document.querySelector("#id1");
+// document.querySelector(".list");
+// document.querySelector("li a");
+// document.querySelector("a");
+// document.querySelector("li.list");
+// document.querySelector("#id2 a");
+// document.querySelector("#id2 .list");
+// document.querySelectorAll("#id2 .list");
+// document.querySelectorAll("#id2 .list")[2].style.color = "blue";
+// document.querySelector("li a").style.color = "red";
+// document.querySelector("li").style.color = "red";
+
+// PART - 2
+// document.querySelector("button").classList;
+// document.querySelector("button").classList.add("invisible");
+// document.querySelector("button").classList.remove("invisible");
+// document.querySelector("button").classList.toggle("invisible");
+// document.querySelector("#id1").textContent = "Hiii...";
+// document.querySelector("h1").innerHTML;
+// document.querySelector("h1").innerHTML = "<em>Good Bye</em>";
+// document.querySelector("h1").textContent = "Good Bye";
+
+// PART - 3
+// document.querySelector("a");
+// document.querySelector("a").attributes;
+// document.querySelector("a").getAttribute("href");
+// document.querySelector("a").setAttribute("href", "https://www.bing.com");
